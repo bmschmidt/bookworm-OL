3d7aff87fd52cb65cfe3484d5e93afa21268877e
Benjamin Schmidt <bmschmidt@gmail.com>
2014-06-17 14:37:31 -0400

    Adding script to rewrite IA books with regexes into a more text-appropriate format

---
+++ OLprinter.py
@@ -0,0 +1,42 @@
+#! /usr/bin/python
+import sys
+from parsingClasses import *
+
+files = sys.argv[1:]
+
+#Some regular expressions that are us
+
+def dropNewlines(text):
+    return re.sub(r"[\n\r\t]"," ",text)
+
+def dropGooglePrelude(text):
+    return re.sub(r"^.{1000,8000}: ?/ ?/ ?book ?s ?\. ?[qg]oo[qg][liI]e ?\. ?co.","",text)
+
+
+
+def removeIAattribution(text):
+    return re.sub(r"^.{0,50}Digitized.{5,1000}(h|li)ttp\S+","",text)
+
+def closeHyphens(text):
+    return re.sub(r"- *\n([\n ])*","",text)
+
+def printout(filename):
+    mine = ocaid(filename)
+    text = open("files/texts/raw/" + mine.fileLocation()).read()
+    text = closeHyphens(text)
+    text = dropNewlines(text)
+    text = removeIAattribution(text)
+    text = dropGooglePrelude(text)
+    if text[:5] == "<html" or text[:5] == "<!DOC":
+        raise TypeError("it's HTML")
+    return text
+
+for file in files:
+    try:
+        text = printout(file)
+        print file + "\t" + text
+    except TypeError:
+        pass
+        
+
+
